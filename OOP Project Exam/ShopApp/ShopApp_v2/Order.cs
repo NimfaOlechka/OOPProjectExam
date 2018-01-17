2691556da4e06221cd35af6928386aa314dd1869
Olga K <nimfaolya@mail.ru>
2018-01-17 13:04:02 +0100

    shop version 2 app added - order class separately function example

---
+++ OOP Project Exam/ShopApp/ShopApp_v2/Order.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp_v2
+{
+    class Order
+    {
+        //variables of order
+        public delegate void MessageDelegate(string message);
+
+        double sum; //keeping sum of the order
+        protected int orderId;//automatisk id
+        static int idCount = 0;// for setting unique id for each order
+        double discount;//discount from order
+        string customerName;
+
+        MessageDelegate mesDel;
+
+        public void RegisterMessageDelegate(MessageDelegate mes)
+        {
+            mesDel = mes;
+        }
+
+        public Order()//constructor
+        {
+            sum = 0;
+            discount = 0.0;
+            orderId = ++idCount;
+            customerName = "Default customer";
+        }
+
+        public double CurrentSum
+        {
+            get { return sum; }
+        }
+
+        // add or delete goods from basket
+        public void AddItem(double price)
+        {
+
+            sum += price;
+            //discount = sum*discount;
+            //sum -= discount;
+            mesDel($"You added an item on sum: {price}");
+            //mesDel($"Sum of ur order is: {sum}");
+        }
+
+        public void DeleteItem(double price)
+        {
+            if (price <= sum)
+            {
+                sum -= price;
+                //sum += discount*10;
+                mesDel($"You deleted item on sum: {price}");
+                //mesDel($"Sum of ur order is: {sum}");
+            }
+            else
+            {
+                mesDel("Impossible to delete unexisted!");
+            }
+
+        }
+
+        public void GetDiscount(string customertype)
+        {
+
+            switch (customertype)
+            {
+                case "base":
+                    discount = 0.0;
+                    break;
+
+                case "start":
+                    discount = 0.05;
+                    break;
+                case "middle":
+                    discount = 0.1;
+                    break;
+                case "big":
+                    discount = 0.2;
+                    break;
+                default:
+                    discount = 0.0;
+                    break;
+            }
+             mesDel($"Your discount is: {discount:P}");
+             discount = sum * discount;
+             sum -= discount;
+             mesDel($"Sum of ur order is {sum:C}");
+           
+        }
+
+        public override string ToString()
+        {
+            return ($"Order #: {orderId} made {customerName} on Total Sum: {sum:C}. discount made: {discount:P}");
+        }
+    }
+}
---
+++ OOP Project Exam/ShopApp/ShopApp_v2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Order> orders = new List<Order>();
+            
+
+            Console.WriteLine("Shop <BUY SOMETHING>");
+
+            // create temporary object to work with
+            Order tempOrder = new Order();
+
+            tempOrder.RegisterMessageDelegate((string s) =>
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(s);
+                Console.ResetColor();
+            });
+            Console.Write("Write [base; start; middle or big]: ");
+            string type = Console.ReadLine().ToLower();
+            
+            bool alive = true;
+            while (alive)
+            {
+                
+                //menu to choose 
+                ConsoleColor color = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("\n1. Buy something \t2. Delete something \t3. Show existed orders");
+                Console.WriteLine("4. Count Sum \t5. Close Shop");
+                Console.Write("Enter number of choice:");
+                Console.ForegroundColor = color;
+                try
+                {
+                    int command = Convert.ToInt32(Console.ReadLine());
+
+                    switch (command)
+                    {
+                        case 1:
+                            tempOrder.AddItem(5);
+                            break;
+                        case 2:
+                            tempOrder.DeleteItem(5);
+                            break;
+                        case 3:
+                            orders.Add(tempOrder);
+                            foreach(Order o in orders)
+                            {
+                                Console.WriteLine(o);
+                            }
+                            
+                            break;
+                        case 4:
+                            tempOrder.GetDiscount(type);
+                            break;
+                        case 5:
+                            alive = false;
+                            continue;
+                    }
+                    //DoSomething();
+                }
+                catch (Exception ex)
+                {
+                    color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ForegroundColor = color;
+                }
+            }
+            //Console.ReadKey();
+        }
+
+        private static void ShowExistedOrders()
+        {
+            Console.Write("nothing to show yet!");
+        }
+
+        //private static void DeleteItem(Order order)
+        //{
+            
+        //}
+
+        //private static void CreateOrder()
+        //{
+            
+            
+        //}
+    }
+}
